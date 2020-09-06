d330d318ff8b3be90c3b47aee29939bdd1b91f95
nick L <xnel417x@gmail.com>
2020-09-05 19:42:28 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"html/template"
+	"net/http"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+)
+
+var client *redis.Client
+var templates *template.Template
+
+func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler).Methods("GET")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.TODO()
+	comments, err := client.LRange(ctx, "comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
