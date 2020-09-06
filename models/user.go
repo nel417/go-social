31bf59da66e942a3a350d7e104b9768c014b902e
nick L <xnel417x@gmail.com>
2020-09-06 16:57:58 -0500

    updated user model

--- models/user.go
+++ models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/go-redis/redis"
 	"golang.org/x/crypto/bcrypt"
@@ -13,21 +14,77 @@ var (
 	ErrInvalidLogin = errors.New("invalid login")
 )
 
-func AuthenticateUser(username, password string) error {
-	ctx := context.TODO()
-	hash, err := client.Get(ctx, "user:"+username).Bytes()
-	if err == redis.Nil {
-		return ErrUserNotFound
+type User struct {
+	key string
+}
 
-	} else if err != nil {
+func NewUser(username string, hash []byte) (*User, error) {
+	ctx := context.TODO()
+
+	id, err := client.Incr(ctx, "user:next-id").Result()
+	if err != nil {
+		return nil, err
+	}
+	key := fmt.Sprintf("user:%d", id)
+	pipe := client.Pipeline()
+	pipe.HSet(ctx, key, "id", id)
+	pipe.HSet(ctx, key, "username", username)
+	pipe.HSet(ctx, key, "hash", hash)
+	pipe.HSet(ctx, "user:by-username", username, id)
+	_, err = pipe.Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &User{key}, nil
+}
+
+func (user *User) GetUsername() (string, error) {
+	ctx := context.TODO()
+
+	return client.HGet(ctx, user.key, "username").Result()
+}
+
+func (user *User) GetHash() ([]byte, error) {
+	ctx := context.TODO()
+
+	return client.HGet(ctx, user.key, "hash").Bytes()
+}
+
+func (user *User) Authenticate(password string) error {
+	hash, err := user.GetHash()
+	if err != nil {
 		return err
 	}
-
 	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
-	if err != nil {
+	if err == bcrypt.ErrMismatchedHashAndPassword {
 		return ErrInvalidLogin
 	}
-	return nil
+	return err
+
+}
+
+func GetUserByUsername(username string) (*User, error) {
+	ctx := context.TODO()
+
+	id, err := client.HGet(ctx, "user:by-username", username).Int64()
+	if err == redis.Nil {
+		return nil, ErrUserNotFound
+
+	} else if err != nil {
+		return nil, err
+	}
+	key := fmt.Sprintf("user:%d", id)
+	return &User{key}, nil
+
+}
+
+func AuthenticateUser(username, password string) error {
+	user, err := GetUserByUsername(username)
+	if err != nil {
+		return err
+	}
+	return user.Authenticate(password)
+
 }
 
 func RegisterUser(username, password string) error {
@@ -37,6 +94,6 @@ func RegisterUser(username, password string) error {
 	if err != nil {
 		return err
 	}
-	ctx := context.TODO()
-	return client.Set(ctx, "user:"+username, hash, 0).Err()
+	_, err = NewUser(username, hash)
+	return err
 }
